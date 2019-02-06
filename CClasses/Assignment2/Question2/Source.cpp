f868c38ec5f0624c0c35a20c54c986f0c75fc736
keerthanareddyvarakala <keerthanavarakala@gmail.com>
2019-02-06 11:36:39 -0800

    prime changed

--- CClasses/Assignment2/Question2/Source.cpp
+++ CClasses/Assignment2/Question2/Source.cpp
@@ -1,11 +1,21 @@
 #include<stdio.h>
 int main()
 {
-	int n, m, i, f = 0;
+	int n, m, i, f = 0, j;
 	printf("enter 1st num");
 	scanf("%d", &n);
 	printf("enter 2nd num");
 	scanf("%d", &m);
-
+	for (i = n + 1; i < m; i++)
+	{
+		for (j = 2; j <= i / 2; j++)
+		{
+			if (i%j == 0)
+				f = 1;
+		}
+		if (f == 0)
+			printf("\n %d", i);
+		f = 0;
+	}
 	return 0;
 }
