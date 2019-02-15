4f2540d9c855bd01dc538f2a4c8815e13c0506b9
SakethaGaddam <gaddam.saketha2gmail.com>
2019-02-15 13:26:39 -0800

    Question7

--- C++/Assignment1/Question7/main.cpp
+++ C++/Assignment1/Question7/main.cpp
@@ -44,4 +44,50 @@ public:
 				cin >> *(*(ptr+i)+j);
 			}
 	}
+	void display()
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				cout << " " << *(*(ptr + i) + j) << " ";
+			}
+			cout << endl;
+		}
+	}
+	friend matrix matrix_multiplication(matrix, matrix);
 };
+matrix matrix_multiplication(matrix m1, matrix m2)
+{
+	int sum = 0, i, j, k;
+	matrix m(m1.rows, m2.cols);
+	if (m1.cols != m2.rows)
+		cout << "matrix multiplication not possible" << endl;
+	else
+	{
+		
+		for (i = 0; i < m1.rows; i++)
+		{
+			for (j = 0; j < m2.cols; j++)
+			{
+				for (k = 0; k < m2.rows; k++)
+				{
+					sum = sum+(*(*(m1.ptr + i) + k) )* (*(*(m2.ptr + k) + j));
+				}
+				*(*(m.ptr + i) + j) = sum;
+				sum = 0;
+			}
+			
+		}
+	}
+	return m;
+}
+int main()
+{
+	matrix m1(2, 3), m2(3, 2), m3;
+	m1.matrix_initialize();
+	m2.matrix_initialize();
+	m3 = matrix_multiplication(m1, m2);
+	m3.display();
+	return 0;
+}
