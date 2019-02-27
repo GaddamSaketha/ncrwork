54b2445a95c03f2ce9452bb6f05c218e1e83904d
SakethaGaddam <gaddam.saketha2gmail.com>
2019-02-27 09:16:11 -0800

    changed ques1

--- Windows/Question1/main.cpp
+++ Windows/Question1/main.cpp
@@ -1,16 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<windows.h>
 #include<string.h>
 #include<stdlib.h>
 int main()
 {
+	int arr_len, arr1_len;
 	char c = 'A';
 	wchar_t c1 = L'A';
 	CHAR C = 'B';
-	WCHAR C1 = 'B';
-	CHAR ARR[] = "ANSI";
-	WCHAR ARR1[] = L"WCHAR";
-	
+	WCHAR C1 = L'B';
+	printf("enter length of char array\n");
+	scanf("%d", &arr_len);
+	PSTR ARR = (PSTR)malloc(sizeof(CHAR)*arr_len);
+	printf("enter char array\n");
+	scanf("%s", ARR);
+	printf("enter wide char array length\n");
+	scanf("%d", &arr1_len);
+	PWSTR ARR1 = (PWSTR)malloc(sizeof(WCHAR)*arr1_len);
+	printf("enter wide char array\n");
+	scanf("%S", ARR1);
 	printf("is ARR an unicode: %d\n", IsTextUnicode(ARR, strlen(ARR)+1, NULL));
 	printf("is ARR1 an unicode: %d\n", IsTextUnicode(ARR1, wcslen(ARR1)+1, NULL));
 	int res = MultiByteToWideChar(CP_UTF8,0,ARR, strlen(ARR),NULL,0);
@@ -33,10 +42,10 @@ int main()
 	PSTR RES_ARR1 = (PSTR)malloc(sizeof(CHAR)*res);
 	WideCharToMultiByte(CP_UTF8, 0, ARR1, wcslen(ARR1)+1, RES_ARR1, strlen(RES_ARR1), NULL, NULL);
 	printf("%s \n", RES_ARR1);
-	TCHAR char1 = TEXT(C);
-	TCHAR char2 = TEXT(C1);
-	printf("size of char1: %d\n", sizeof(char1));
-	printf("size of char2: %d\n", sizeof(char2));
+	//TCHAR char1 = TEXT(C);
+	//TCHAR char2 = TEXT(C1);
+	//printf("size of char1: %d\n", sizeof(char1));
+	//printf("size of char2: %d\n", sizeof(char2));
 	
 	return 0;
 }
