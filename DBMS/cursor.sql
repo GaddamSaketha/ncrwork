19908020bec177ade03838ae15ed004891227eec
SakethaGaddam <gaddam.saketha2gmail.com>
2019-03-18 11:03:46 -0700

    updated

--- DBMS/cursor.sql
+++ DBMS/cursor.sql
@@ -1,10 +1,10 @@
 use sampp;
 DECLARE
-@total_wages int
-@sal_count int
-@comm_count int
-@wages int
-@salary int
+@total_wages int,
+@sal_count int,
+@comm_count int,
+@wages int,
+@salary int,
 @commission int
 DECLARE db_cursor CURSOR FOR
 select sal,isnull(comm,0),sal+isnull(comm,0)"wages" from emp where deptno=20;
@@ -12,13 +12,19 @@ OPEN db_cursor
 FETCH NEXT FROM db_cursor INTO @salary,@commission,@wages
 WHILE @@FETCH_STATUS = 0  
 BEGIN  
+set @total_wages=0
+set @sal_count=0
+set @comm_count=0
 if(@salary>2000)
-set @sal_count=@sal_count+1;
+set @sal_count=@sal_count+1
 if(@commission>@salary)
-set @comm_count=@comm_count+1;
-set @total_wages=@total_wages+@wages;
+set @comm_count=@comm_count+1
+set @total_wages=@total_wages+@wages
 
 END  
+print @total_wages
+print @sal_count
+print @comm_count
 CLOSE db_cursor  
 DEALLOCATE db_cursor 
 
